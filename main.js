afe2be046b3c097de540ec511ad2ced8d1f374d4
Jason Davis <Jasonodavis323@gmail.com>
2022-07-07 16:23:12 -0600

    lab complete

---
+++ main.js
@@ -0,0 +1,27 @@
+//const { default: axios } = require("axios")
+
+//const { default: axios } = require("axios")
+
+//const { default: axios } = require("axios")
+
+// ask jeddy: why does using the query not work??   ... "  ?search=Alderaan"   ""
+//asl jeddy: what is the above const variables about??
+const baseUrl = "https://swapi.dev/api/planets/2/"
+const button = document.querySelector("button")
+function resButton()  {
+    console.log('button clicked')
+    axios.get(baseUrl).then((res) => {
+        let residents = res.data.residents
+        for(i = 0; i < residents.length; i ++) {
+            axios.get(residents[i]).then((res) => {
+                console.log(res.data.name)
+                let names = document.createElement('h2')
+                names.textContent = res.data.name
+                document.querySelector("body").appendChild(names)
+            })
+        }
+
+    })
+   
+}
+button.addEventListener('click', resButton)
